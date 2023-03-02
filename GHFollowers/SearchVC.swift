a09ed95ef8b4fbe63263383571333f828b8f7754
Ben Reeps <reepscodes@gmail.com>
2023-03-01 22:01:27 -0500

    setup constraints

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -9,22 +9,35 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let logoImageView       = UIImageView()
+    let usernameTextField   = GFTextField()
+    let callToActionButton  = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    // generic button used incase name changes
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemPink
-
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        // Use viewWillAppear bc viewDidLoad only gets called the first time the view loads. It would not get called if user were to go to followrs then back unless using viewWillAppear
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
     }
-    */
-
+    
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        // "stringley typed" would be a typo in gh-logo that would casue crash
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 }
