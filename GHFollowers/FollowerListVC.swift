266f3de9b78ce90d43ce98d0881b92f1a540de7b
Ben Reeps <reepscodes@gmail.com>
2023-03-03 15:23:02 -0500

    create followerListVC

---
+++ GHFollowers/FollowerListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FollowerListVCViewController.swift
+//  GHFollowers
+//
+//  Created by Benjamin Reeps on 3/3/23.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
