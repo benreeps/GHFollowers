b7c25da632269e9606835facb495cf67668f5d58
Ben Reeps <reepscodes@gmail.com>
2023-03-05 16:23:01 -0500

    Create follower model

--- GHFollowers/Model/Follower.swift
+++ GHFollowers/Model/Follower.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct Followers: Codable {
+    var login: String
+    // login is not going to be null bc github will not allow this edge case 
+    var avatarUrl: String
+    // Codable will automatically correct camel case avatarUrl into snake case avatar_url
+}
