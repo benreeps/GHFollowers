1ca2e9dfbe5572e19efb70839d566a1e44823859
Ben Reeps <reepscodes@gmail.com>
2023-03-05 09:04:38 -0500

    initialize title and body label

---
+++ GHFollowers/GFBodyLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by Benjamin Reeps on 3/4/23.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
