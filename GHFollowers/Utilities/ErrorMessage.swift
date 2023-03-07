54046756afee534cd36d829a5820d3a037297ca3
Ben Reeps <reepscodes@gmail.com>
2023-03-06 21:31:18 -0500

    refactor result type

--- GHFollowers/Utilities/ErrorMessage.swift
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -8,7 +8,7 @@
 import Foundation
 // Associated values - type is declared after each case
 // Raw value - Type is defined after the enum and then all of your cases conform to that one type. The abaove enuum is a raw value
-enum ErrorMessage: String {
+enum GFError: String, Error {
     case invalidUsername    = "This username created an invalidrequest. Please try again."
     case unableToComplete   = "Unable to complete your request. Please check your internet connection"
     case invalidResponse    = "Invalid response from the server. Please try again."
