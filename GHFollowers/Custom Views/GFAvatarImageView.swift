fe85a793d5d1082c450599f45e2d32734ba0b671
Ben Reeps <reepscodes@gmail.com>
2023-03-17 20:19:52 -0400

    network call for download image

--- GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -21,11 +21,32 @@ class GFAvatarImageView: UIImageView {
     }
     
     
-    
     private func configure() {
         layer.cornerRadius   = 10
         clipsToBounds        = true
         image                = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    func downloadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
+            if error != nil {return}
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {return}
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                // Any time you update the UI you have to do it on the main thread
+                self.image = image
+            }
+            
+        }
+        task.resume()
     }
 }
