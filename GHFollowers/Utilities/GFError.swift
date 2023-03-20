bdddb9d378aeaf0b9eddb16a1fc4a4caa0db7896
Ben Reeps <reepscodes@gmail.com>
2023-03-20 17:48:54 -0400

    rename GFError file by deletion

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -1,17 +1,15 @@
 //
-//  ErrorMessage.swift
+//  GFError.swift
 //  GHFollowers
 //
-//  Created by Benjamin Reeps on 3/6/23.
+//  Created by Benjamin Reeps on 3/20/23.
 //
 
 import Foundation
-// Associated values - type is declared after each case
-// Raw value - Type is defined after the enum and then all of your cases conform to that one type. The abaove enuum is a raw value
+
 enum GFError: String, Error {
     case invalidUsername    = "This username created an invalidrequest. Please try again."
     case unableToComplete   = "Unable to complete your request. Please check your internet connection"
     case invalidResponse    = "Invalid response from the server. Please try again."
     case invalidData        = "The data received from the server is invalid. Please try again."
 }
-
