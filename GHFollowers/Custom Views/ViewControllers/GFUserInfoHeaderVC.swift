75ea2b86e09e9284e500b9dc7977e0265c5f40fa
Ben Reeps <reepscodes@gmail.com>
2023-03-21 22:11:27 -0400

    add secondary title label

---
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,17 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers
+//
+//  Created by Benjamin Reeps on 3/21/23.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+}
