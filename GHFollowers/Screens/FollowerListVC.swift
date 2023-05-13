7dfe06dfdf95c309ee0fde9902fc77666d16f35f
Ben Reeps <reepscodes@gmail.com>
2023-05-13 15:14:04 -0400

    optimize collection view scroll to top

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -208,6 +208,7 @@ extension FollowerListVC: FollowerListVCDelegate {
         followers.removeAll()
         filteredFollowers.removeAll()
         collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
         getFollowers(username: username, page: page)
     }
 }
